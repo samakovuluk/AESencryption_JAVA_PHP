ff55284d1c4a45fa2fc99ae99d18557fe3ea7edb
Undeath <35860953+samakovuluk@users.noreply.github.com>
2018-11-03 17:56:33 +0600

    Update DemoAES.php

--- PHP/DemoAES.php
+++ PHP/DemoAES.php
@@ -1,10 +1,9 @@
 <?php
 $i=$_POST["data"];
 $i=str_replace(" ", "+", $i);
-
+//echo $i;
 //require_once('vendor/autoload.php');
 include("AES.php");
-
 $aes = new AES();
 $aes->setKey("1234567890123456");
 $aes->setData($i);
@@ -14,58 +13,146 @@ $aes->setIv($ivString);
 //$encryptedString = $aes->encrypt();
 //$aes->setData($encryptedString);
 $decryptedString = $aes->decrypt();
+//echo $decryptedString;
+//echo "----";
 $data = explode("*---*", $decryptedString);
-//var_dump($data);
 $opt= explode(",", $data[0]);
 $url=$opt[0];
 $mth=$opt[1];
 $crd=$opt[2];
-echo "$url";
-echo "\n";
-echo "$mth";
-echo "\n";
-echo "$crd";
-echo "\n";
-echo "$data[1]";
 
-echo "--------------------------------------------";
-$request = new HttpRequest();
-$request->setUrl($url);
-$request->setMethod(HTTP_METH_POST);
-$request->setHeaders(array(
-  'cache-control' => 'no-cache',
-  'authorization' => $crd,
-  'content-type' => 'application/json'
-));
-$request->setBody($data[1]);
-$res="";
-try {
-  $response = $request->send();
-  $res=$response->getBody();
-} catch (HttpException $ex) {
-  $res=$ex;
+if ($mth=="POST") {
+//$url= urlencode($url);
+$url=str_replace("%3A", ":", $url);
+$url=str_replace("%2F", "/", $url);
+$url=str_replace("%3F", "?", $url);
+$url=str_replace("%24", "$", $url);
+$url=str_replace("%3D", "=", $url);
+
+
+
+
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => $mth,
+
+    CURLOPT_POSTFIELDS => $data[1],
+
+    CURLOPT_HTTPHEADER => array(
+      "authorization: ".$crd,
+      "cache-control: no-cache",
+      "content-type: application/json",
+    ),
+  ));
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    $data=htmlentities($response);
+    $data=str_replace("&quot;","\"",$data);
+	$aes->setData($data);
+	$decryptedString = $aes->encrypt();
+    echo $decryptedString;
+
+  }
+
+  // code...
 }
-echo "$res";
+else if ($mth=="GET") {
+/*$url= urlencode($url);
+$url=str_replace("%3A", ":", $url);
+$url=str_replace("%2F", "/", $url);
+$url=str_replace("%3F", "?", $url);
+$url=str_replace("%24", "$", $url);
+$url=str_replace("%3D", "=", $url);*/
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => $mth,
+
+    CURLOPT_POSTFIELDS => $data[1],
+
+    CURLOPT_HTTPHEADER => array(
+      "authorization: ".$crd,
+      "cache-control: no-cache",
+      "content-type: application/json",
+    ),
+  ));
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    $data=htmlentities($response);
+    $data=str_replace("&quot;","\"",$data);
+	$aes->setData($data);
+	$decryptedString = $aes->encrypt();
+    echo $decryptedString;
+
+  }
+
+  // code...
+}
+else if ($mth=="PATCH") {
+
+/*$url= urlencode($url);
+$url=str_replace("%3A", ":", $url);
+$url=str_replace("%2F", "/", $url);
+$url=str_replace("%3F", "?", $url);
+$url=str_replace("%24", "$", $url);
+$url=str_replace("%3D", "=", $url);*/
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => $mth,
+
+    CURLOPT_POSTFIELDS => $data[1],
+
+    CURLOPT_HTTPHEADER => array(
+      "authorization: ".$crd,
+      "cache-control: no-cache",
+      "content-type: application/json",
+    ),
+  ));
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    $data=htmlentities($response);
+    $data=str_replace("&quot;","\"",$data);
+	$aes->setData($data);
+	$decryptedString = $aes->encrypt();
+    echo $decryptedString;
+  }
+
+  // code...
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//echo "Available methods: " . implode(",", $aes->getAvailableMethods()) . "<br/><br/>" . PHP_EOL;
-//echo "IV string: " . $ivString . "<br/>" . PHP_EOL;
-//echo "Encrypted string: " . $encryptedString . "<br/>" . PHP_EOL;
-//echo "Decrypted string: " . $decryptedString . "<br/>" . PHP_EOL;
